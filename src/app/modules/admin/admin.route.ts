75dddcb3750a7e1743e3b36758044ebafa05732e
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-25 14:20:14 +0600

    h

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -5,6 +5,7 @@ import { blockUserValidationSchema } from "../user/user.validation";
 import { userController } from "../user/auth.controller";
 import { auth } from "../../middleware/auth";
 import { validateRequest } from "../../middleware/validateRequest";
+import { bikeController } from "../bike/bike.controller";
 
 const router = express.Router();
 
@@ -16,6 +17,11 @@ router.patch(
   validateRequest(blockUserValidationSchema),
   userController.userBlockUpdate
 );
- 
+router.post(
+  "/products",
+  auth.authUser,
+  auth.onlyAdmin,
+  bikeController.createBike
+);
 
 export const AdminRoute = router;
--- src/app/modules/bike/bike.router.ts
+++ src/app/modules/bike/bike.router.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { bikeController } from './bike.controller'
 const router = express.Router()
 
-router.post('/products',bikeController.createBike)
+
 router.get('/products/:productId',bikeController.getSingleBike)
 router.get('/products',bikeController.getBike)
 router.delete('/products/:productId',bikeController.deleteBike)
